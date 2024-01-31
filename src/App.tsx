54972ebfbf6246be594356a30a95295d1b553a2b
נועם מועלם <40291455+NoamMuallem@users.noreply.github.com>
2024-01-31 21:32:23 +0000

    basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      id="root"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+        justifyContent: "start",
+        alignItems: "start",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <h1>My Awsome Pokemon Selector</h1>
+    </div>
   );
 }
 
