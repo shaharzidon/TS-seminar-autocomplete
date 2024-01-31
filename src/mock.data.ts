c2e5eef9ad9e8eee745b162be62006de4d50a650
נועם מועלם <40291455+NoamMuallem@users.noreply.github.com>
2024-01-31 21:27:41 +0000

    added data with type

---
+++ src/mock.data.ts
@@ -0,0 +1,69 @@
+export type Pokemon = {
+  type: string;
+  isLegendary: boolean;
+  name: string;
+  pokedexEntryNumber: number;
+};
+
+export const pokemon = [
+  {
+    type: "Fire",
+    isLegendary: false,
+    name: "Charmander",
+    pokedexEntryNumber: 4,
+  },
+  {
+    type: "Water",
+    isLegendary: false,
+    name: "Squirtle",
+    pokedexEntryNumber: 7,
+  },
+  {
+    type: "Grass",
+    isLegendary: false,
+    name: "Bulbasaur",
+    pokedexEntryNumber: 1,
+  },
+  {
+    type: "Electric",
+    isLegendary: false,
+    name: "Pikachu",
+    pokedexEntryNumber: 25,
+  },
+  {
+    type: "Psychic",
+    isLegendary: true,
+    name: "Mewtwo",
+    pokedexEntryNumber: 150,
+  },
+  {
+    type: "Dragon",
+    isLegendary: true,
+    name: "Rayquaza",
+    pokedexEntryNumber: 384,
+  },
+  {
+    type: "Ice",
+    isLegendary: false,
+    name: "Glaceon",
+    pokedexEntryNumber: 471,
+  },
+  {
+    type: "Fairy",
+    isLegendary: false,
+    name: "Sylveon",
+    pokedexEntryNumber: 700,
+  },
+  {
+    type: "Ghost",
+    isLegendary: false,
+    name: "Gengar",
+    pokedexEntryNumber: 94,
+  },
+  {
+    type: "Steel",
+    isLegendary: true,
+    name: "Dialga",
+    pokedexEntryNumber: 483,
+  },
+];
