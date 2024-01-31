26c47f88d4c7e695cbbd90f6f499e055006757b7
נועם מועלם <40291455+NoamMuallem@users.noreply.github.com>
2024-01-31 22:22:51 +0000

    just added states and hooks for the autocomplete

--- src/components/autocomplete/index.tsx
+++ src/components/autocomplete/index.tsx
@@ -1,11 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+type MultiOrSingular<T> =
+  | { isMulty: true; onChange: (value: T[]) => void }
+  | { isMulty: false; onChange: (value: T) => void };
 
 type AutocompleteProps<T> = {
+  isMulty: boolean;
   options: T[];
   getOptionLabel: (option: T) => string;
   getOptionID: (option: T) => string;
-};
+} & MultiOrSingular<T>;
 
-export const Autocomplete = <T,>({ options }: AutocompleteProps<T>) => {
-  const [selectedOptions, setSelectedOptions] = useState<>([]);
+export const Autocomplete = <T,>({
+  options,
+  getOptionID,
+  isMulty,
+  onChange,
+}: AutocompleteProps<T>) => {
+  const [selectedOptions, setSelectedOptions] = useState<Record<string, T>>({});
+
+  const toggleOption = (option: T) => {
+    const id = getOptionID(option);
+    setSelectedOptions((lastState) => {
+      const isOptionsSelected = lastState[id];
+      if (isMulty) {
+        //multi select
+        //already selected
+        if (isOptionsSelected) {
+          const copy = { ...lastState };
+          delete copy[id];
+          return copy;
+        }
+        //not selected
+        return {
+          ...lastState,
+          [id]: option,
+        };
+      }
+      //single select
+      if (isOptionsSelected) {
+        //already selected
+        return lastState;
+      }
+      //not selected
+      return {
+        [id]: option,
+      };
+    });
+  };
+
+  //
+  useEffect(() => {
+    if (isMulty) {
+      onChange(Object.values(selectedOptions));
+    } else {
+      onChange(Object.values(selectedOptions)[0]);
+    }
+  }, [selectedOptions, isMulty, onChange]);
 };
