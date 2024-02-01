4b8cb7969bd878dd7a4ef935701430d7615143c8
נועם מועלם <40291455+NoamMuallem@users.noreply.github.com>
2024-02-01 12:10:11 +0000

    feat: Implement strict enums and build type for Pokemon type

--- src/mock.data.ts
+++ src/mock.data.ts
@@ -19,6 +19,8 @@ export enum PokemonTypes {
   Steel = "steel",
 }
 
+// // 9. version 2, use strict enums, build a type for Pokemon type
+
 // 2. Array type with generic Pokemon type
 export const pokemons: Array<Pokemon> = [
   {
