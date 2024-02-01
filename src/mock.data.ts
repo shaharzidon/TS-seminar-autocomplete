c70debe53215f679690f22514a040183f07bcf15
נועם מועלם <40291455+NoamMuallem@users.noreply.github.com>
2024-02-01 10:51:59 +0000

    feat: Add enum Types and replace type strings with Types enums

--- src/mock.data.ts
+++ src/mock.data.ts
@@ -5,59 +5,72 @@ export type Pokemon = {
   pokedexEntryNumber: number;
 };
 
+enum Types {
+  Fire = "fire",
+  Water = "water",
+  Grass = "grass",
+  Electric = "electric",
+  Psychic = "psychic",
+  Dragon = "dragon",
+  Ice = "ice",
+  Fairy = "fairy",
+  Ghost = "ghost",
+  Steel = "steel",
+}
+
 export const pokemons: Pokemon[] = [
   {
-    type: "Fire",
+    type: Types.Fire,
     name: "Charmander",
     pokedexEntryNumber: 4,
   },
   {
-    type: "Water",
+    type: Types.Water,
     name: "Squirtle",
     pokedexEntryNumber: 7,
   },
   {
-    type: "Grass",
+    type: Types.Grass,
     name: "Bulbasaur",
     pokedexEntryNumber: 1,
   },
   {
-    type: "Electric",
+    type: Types.Electric,
     isLegendary: false,
     name: "Pikachu",
     pokedexEntryNumber: 25,
   },
   {
-    type: "Psychic",
+    type: Types.Psychic,
     isLegendary: true,
     name: "Mewtwo",
     pokedexEntryNumber: 150,
   },
   {
-    type: "Dragon",
+    type: Types.Dragon,
     isLegendary: true,
     name: "Rayquaza",
     pokedexEntryNumber: 384,
   },
   {
-    type: "Ice",
+    type: Types.Ice,
     name: "Glaceon",
     pokedexEntryNumber: 471,
   },
   {
-    type: "Fairy",
+    type: Types.Fairy,
     isLegendary: false,
     name: "Sylveon",
     pokedexEntryNumber: 700,
   },
   {
-    type: "Ghost",
+    type: Types.Ghost,
     isLegendary: false,
     name: "Gengar",
     pokedexEntryNumber: 94,
   },
   {
-    type: "Steel",
+    type: Types.Steel,
     isLegendary: true,
     name: "Dialga",
     pokedexEntryNumber: 483,
